b25a069a711bdfbbc7d3cbf36fadcb62dfe29b94
Mithilesh Maurya <mauryamithilesh1@gmail.com>
2026-08-19 23:59:15 +0530

    Widen Notification.message to TextField

--- tasks/models.py
+++ tasks/models.py
@@ -135,7 +135,7 @@ class Notification(models.Model):
         on_delete=models.CASCADE,
         related_name="notifications",
     )
-    message = models.CharField(max_length=255)
+    message = models.TextField()
     is_read = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
